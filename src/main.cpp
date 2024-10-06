f07d21505489fc2785ea18f58362ed1f3106396e
El Maco <marcus.myllyviita@gmail.com>
2024-10-06 11:05:56 +0300

    Add icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,41 @@
 #include <windows.h>
+#include "../include/resource.h"
 
-LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
-  switch (msg) {
-  case WM_DESTROY:
-    PostQuitMessage(0);
-    break;
-  default:
-    return DefWindowProc(hwnd, msg, wp, lp);
-  }
-  return 0;
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+    return 0;
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args,
-                   int ncmdshow) {
-  WNDCLASSW wc = {0};
-  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wc.hInstance = hInst;
-  wc.lpszClassName = L"myWindowClass";
-  wc.lpfnWndProc = WindowProcedure;
+                   int ncmdshow)
+{
+    WNDCLASSW wc = {0};
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
+    wc.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
 
-  if (!RegisterClassW(&wc))
-    return -1;
+        if (!RegisterClassW(&wc)) return -1;
 
-  CreateWindowW(L"myWindowClass", L"Hello, Windows!",
-                WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL,
-                NULL, hInst, NULL);
+    CreateWindowW(L"myWindowClass", L"Hello, Windows!",
+                  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL,
+                  NULL, hInst, NULL);
 
-  MSG msg = {0};
+    MSG msg = {0};
 
-  while (GetMessage(&msg, NULL, NULL, NULL)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
+    while (GetMessage(&msg, NULL, NULL, NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 }
