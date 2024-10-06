c67495b78deb07374f7d90a2271fd00c047955d8
El Maco <marcus.myllyviita@gmail.com>
2024-10-06 11:45:28 +0300

    Add buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <windows.h>
+#include <iostream>
 #include "../include/resource.h"
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg)
     {
+        std::cout << "Got message " << msg << std::endl;
+    case WM_COMMAND:
+        switch (LOWORD(wp))
+        {
+        case 1: // Plus button
+            MessageBoxW(hwnd, L"Plus Button Clicked", L"Info", MB_OK);
+            break;
+        case 2: // Minus button
+            MessageBoxW(hwnd, L"Minus Button Clicked", L"Info", MB_OK);
+            break;
+        case 3: // Button 1
+            MessageBoxW(hwnd, L"Button 1 Clicked", L"Info", MB_OK);
+            break;
+        case 4: // Button 2
+            MessageBoxW(hwnd, L"Button 2 Clicked", L"Info", MB_OK);
+            break;
+        case 5: // Button 3
+            MessageBoxW(hwnd, L"Button 3 Clicked", L"Info", MB_OK);
+            break;
+        case 6: // Button 4
+            MessageBoxW(hwnd, L"Button 4 Clicked", L"Info", MB_OK);
+            break;
+        }
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -25,11 +50,20 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args,
     wc.lpfnWndProc = WindowProcedure;
     wc.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
 
-        if (!RegisterClassW(&wc)) return -1;
+    if (!RegisterClassW(&wc))
+        return -1;
 
-    CreateWindowW(L"myWindowClass", L"Hello, Windows!",
-                  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL,
-                  NULL, hInst, NULL);
+    HWND hwnd = CreateWindowW(L"myWindowClass", L"Hello, Windows!",
+                              WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL,
+                              NULL, hInst, NULL);
+
+    // Create buttons
+    CreateWindowW(L"BUTTON", L"+", WS_VISIBLE | WS_CHILD, 50, 50, 50, 50, hwnd, (HMENU)1, hInst, NULL);
+    CreateWindowW(L"BUTTON", L"-", WS_VISIBLE | WS_CHILD, 120, 50, 50, 50, hwnd, (HMENU)1, hInst, NULL);
+    CreateWindowW(L"BUTTON", L"1", WS_VISIBLE | WS_CHILD, 50, 120, 50, 50, hwnd, (HMENU)1, hInst, NULL);
+    CreateWindowW(L"BUTTON", L"2", WS_VISIBLE | WS_CHILD, 120, 120, 50, 50, hwnd, (HMENU)1, hInst, NULL);
+    CreateWindowW(L"BUTTON", L"3", WS_VISIBLE | WS_CHILD, 190, 120, 50, 50, hwnd, (HMENU)1, hInst, NULL);
+    CreateWindowW(L"BUTTON", L"4", WS_VISIBLE | WS_CHILD, 260, 120, 50, 50, hwnd, (HMENU)1, hInst, NULL);
 
     MSG msg = {0};
 
